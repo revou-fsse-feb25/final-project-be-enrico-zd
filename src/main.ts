9456ba40f6e022f865ef0d14687eda66b6d102c2
enrico-zd <enricozada00@gmail.com>
2025-08-22 21:06:09 +0700

    fix: delete / on cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
-      'https://final-project-fe-enrico-zd.vercel.app/',
+      'https://final-project-fe-enrico-zd.vercel.app',
     ],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
