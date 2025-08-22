ae5e0cf8342162c16b3a0297b623b45d4ef91672
enrico-zd <enricozada00@gmail.com>
2025-08-22 20:59:24 +0700

    feat: add cors fe deploy link

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://final-project-fe-enrico-zd.vercel.app/',
+    ],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
